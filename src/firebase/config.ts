6300dd1392cb357fe6d1ed1586f0794dd1861c43
BLUEBERRY FUNDED TRX-USDT <trxusdt87@gmail.com>
2025-10-27 20:53:38 +0000

    Firebase: Error (auth/api-key-not-valid.-please-pass-a-valid-api-key.):

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,11 @@
+
 // This file will be populated with your Firebase project configuration.
 // IMPORTANT: This is a client-side configuration.
 export const firebaseConfig = {
-  apiKey: "REDACTED",
-  authDomain: "footbet-win.firebaseapp.com",
-  projectId: "footbet-win",
-  storageBucket: "footbet-win.appspot.com",
-  messagingSenderId: "REDACTED",
-  appId: "REDACTED",
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
 };
