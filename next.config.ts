f7a4c4ac121f819e970d29ecece3473644dbe1bc
BLUEBERRY FUNDED TRX-USDT <trxusdt87@gmail.com>
2025-10-27 17:15:40 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -28,6 +28,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'media.api-sports.io',
+        port: '',
+        pathname: '/**',
+      }
     ],
   },
 };
