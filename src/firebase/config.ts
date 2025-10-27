9423c490eb16138125ca65b881ca333248d5d28e
BLUEBERRY FUNDED TRX-USDT <trxusdt87@gmail.com>
2025-10-27 20:19:01 +0000

    Itilize sevis acount ak kle ki nan anvirònman an , mwen deja gen yon baz

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,10 +1,10 @@
 // This file will be populated with your Firebase project configuration.
 // IMPORTANT: This is a client-side configuration.
 export const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT_ID.appspot.com",
-  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
-  appId: "YOUR_APP_ID",
+  apiKey: "REDACTED",
+  authDomain: "footbet-win.firebaseapp.com",
+  projectId: "footbet-win",
+  storageBucket: "footbet-win.appspot.com",
+  messagingSenderId: "REDACTED",
+  appId: "REDACTED",
 };
